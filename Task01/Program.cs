c8e4fa068f94121bc106465ae3a6da4d15483ced
Polina Guney <polinais.90@mail.ru>
2023-02-27 14:21:18 +0300

    created program01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -6,3 +6,18 @@
 // a = 2, b = 10 -> нет 
 // a = 9, b = -3 -> да 
 // a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("Ответ: да");
+}
+
+else 
+{
+    Console.WriteLine("Ответ: нет");
+}
