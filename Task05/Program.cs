daaa7ca602e229fbd32202a64b9d3f16fa9d921e
Polina Guney <polinais.90@mail.ru>
2023-02-27 23:35:46 +0300

    created program05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу,
+// 1. которая на вход принимает одно число (N), 
+// 2. а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine ("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = -number;
+while (index <= number)
+{
+    Console.Write(index+" ");
+    index++;
+}
