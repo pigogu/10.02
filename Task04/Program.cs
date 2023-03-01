66a80c180326bd29bd2195032ef5015eef62cb35
Polina Guney <polinais.90@mail.ru>
2023-03-01 23:04:53 +0300

    changed mistake

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -12,10 +12,7 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число c :");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 Console.Write("max = ");
 Console.WriteLine(max);
-
-
