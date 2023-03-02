bc9fd3dc6de5f6f03a05e390fd359abaa1ce844f
Polina Guney <polinais.90@mail.ru>
2023-03-02 23:17:50 +0300

    createdprograms 14 and 16

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// 14. Напишите программу, которая 
+// 1.принимает на вход число и 
+// 2.проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplycityTwoNumbers(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+if (MultiplycityTwoNumbers(number)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿// 16. Напишите программу, которая 
+// 1.принимает на вход два числа и 
+// 2.проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите первое число :");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (Square(firstNumber, secondNumber)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+
+bool Square(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
+
