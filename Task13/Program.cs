bd6dc623afc4109303a6c9b2493fcb12cc3ac171
Polina Guney <polinais.90@mail.ru>
2023-03-05 14:39:13 +0300

    created program13

---
+++ Task13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая 
+// 1.выводит третью цифру заданного числа или 
+// 2.сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("Третьей цифры нет!");
+else
+System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
