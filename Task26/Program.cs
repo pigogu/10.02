e839d3dc8f4ca1629f40f1eeb0cf4fe75a69a467
Polina Guney <polinais.90@mail.ru>
2023-03-12 22:13:46 +0300

    created 26

---
+++ Task26/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 26: Напишите программу, которая 
+// 1.принимает на вход число и 
+// 2.выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+//тело программы
+int number = Prompt("Введите число: ");
+int diget = CheckDigit(number);
+Console.WriteLine($"В числе {number}, {diget} ");
+
+//методы
+int Prompt(string mess) //метод ввода
+{
+Console.Write(mess);
+int num = Convert.ToInt32(Console.ReadLine());
+return num;
+}
+int CheckDigit(int num)
+{
+int result = 0;
+if (num == 0) return 1;
+else
+{
+while (num != 0)
+{
+num = num / 10;
+result = result + 1;
+}
+}
+return result;
+}
