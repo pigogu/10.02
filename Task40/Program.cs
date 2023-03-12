eb367c5b5639fbaf65217d5a2a15f8d589486890
Polina Guney <polinais.90@mail.ru>
2023-03-13 01:26:33 +0300

    created 40 42

---
+++ Task40/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+Console.WriteLine("Введите три числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Thelengthofthesidesofthetriangle (int num1, int num2, int num3)
+{
+if (number1 + number2 > number3 && number2 + number3 > number1 && number1 + number3 > number2) return true;
+else return false;
+}
+
+bool result=Thelengthofthesidesofthetriangle(number1, number2, number3);
+Console.WriteLine(result ? "Да" : "Нет");
+
+// string TransformationNumber(int num)
+// {
+// string newNum = string.Empty;
+// while (num != 0)
+// {
+// newNum = num % 2 + newNum;
+// num = num / 2;
+// }
+// return newNum;
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string transformationNumber = TransformationNumber(number);
+// Console.WriteLine(transformationNumber);
+// Nikita Bannikov: Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int DecToBin(int num)
+// {
+// int count = 1;
+// int result = 0;
+// while (num > 0)
+// {
+// result = result + num % 2 * count;
+// num = num / 2;
+// count *= 10;
+// }
+// return result;
+// }
+// int decToBin = DecToBin(number);
+// Console.WriteLine($"{number} -> {decToBin}");
---
+++ Task42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите десятичное число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+string ConverDecimalToBinary(int num)
+{
+string number = "";
+
+while (num > 0)
+{
+if (num % 2 == 0)
+{
+number = number + "0";
+num = num / 2;
+} else {
+number = number + "1";
+num = num / 2;
+}
+
+}
+return number;
+}
+
+Console.Write(ConverDecimalToBinary(decimalNumber));
