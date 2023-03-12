9bcf757c2afb6893287b9094a7e6ffdd1858b60a
Polina Guney <polinais.90@mail.ru>
2023-03-12 21:26:41 +0300

    created program18

---
+++ Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+//методы
+string Prompt(string message)
+{
+Console.Write(message);
+string num = Console.ReadLine();
+return num;
+}
+string Range(string num)
+{
+if (num == "1") return "x>0, y>0";
+if (num == "2") return "x<0, y>0";
+if (num == "3") return "x<0, y<0";
+if (num == "4") return "x>0, y<0";
+return "0";
+}
+
+//тело программы
+string number = Prompt("Введите номер четверти ");
+string range = Range(number);
+string result = range=="0" ?
+"Не коректный ввод данных"
+: $" диапазон возможных координат точек в этой четверти {range}";
+Console.WriteLine(result);
